4dc19116cf5f7870ec0e8d9bcb547d31a1ed80ef
Wei Low <weiwlow@gmail.com>
2016-07-26 15:42:04 -0700

    Moved band specific components to BandAPI.cs

--- Samples/Accelerometer/Accelerometer.Shared/MainPage.cs
+++ Samples/Accelerometer/Accelerometer.Shared/MainPage.cs
@@ -33,73 +33,9 @@ namespace Accelerometer
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            this.viewModel.StatusMessage = "Running ...";
-
-            try
-            {
-                // Get the list of Microsoft Bands paired to the device.
-                IBandInfo[] pairedBands = await BandClientManager.Instance.GetBandsAsync();
-                if (pairedBands.Length < 1)
-                {
-                    this.viewModel.StatusMessage = "This sample app requires a Microsoft Band paired to your device. Also make sure that you have the latest firmware installed on your Band, as provided by the latest Microsoft Health app.";
-                    return;
-                }
-
-                // Connect to Microsoft Band.
-                using (IBandClient bandClient = await BandClientManager.Instance.ConnectAsync(pairedBands[0]))
-                {
-                    int samplesReceived = 0; // the number of Accelerometer samples received
-
-                    // Subscribe to Accelerometer data.
-                    bandClient.SensorManager.Accelerometer.ReadingChanged += (s, args) =>
-                    {
-                        samplesReceived++;
-                        int accel = Accelerometer_ReadingChanged(s, args);
-                        if (accel == 2)
-                            bandClient.NotificationManager.VibrateAsync(Microsoft.Band.Notifications.VibrationType.NotificationOneTone);
-                        else
-                        { }
-                    };
-
-                    await bandClient.SensorManager.Accelerometer.StartReadingsAsync();
-
-                    // Receive Accelerometer data for a while, then stop the subscription.
-                    await Task.Delay(TimeSpan.FromSeconds(20));
-                    await bandClient.SensorManager.Accelerometer.StopReadingsAsync();
-                }
-            }
-            catch (Exception ex)
-            {
-                this.viewModel.StatusMessage = ex.ToString();
-            }
+            BandAPI band = new BandAPI();
+            band.StartBandAPI();
         }
-
-        private int Accelerometer_ReadingChanged(object sender, BandSensorReadingEventArgs<IBandAccelerometerReading> e)
-        {
-            int v;
-            IBandAccelerometerReading accel = e.SensorReading;
-            //Read in accelerometer data from sensor
-            string text = string.Format("Accelerometer X = {0:G4}, Y = {1:G4}, Z = {2:G4}", accel.AccelerationX, accel.AccelerationY, accel.AccelerationZ);
-            System.Diagnostics.Debug.WriteLine(text);
-            if (Math.Abs(accel.AccelerationX) > 2.0)
-            {
-                v = 2;
-            }
-            else
-            {
-                v = 1;
-            }
-            return v;
-        }
-
-        private void Gyroscope_ReadingChanged(object sender, BandSensorReadingEventArgs<IBandGyroscopeReading> e)
-        {
-            IBandGyroscopeReading gyro = e.SensorReading;
-            //Read in gyroscope data from sensor
-            string text = string.Format("Gyroscope: X = {0:G4}, Y = {1:G4}, Z = {2:G4}", gyro.AngularVelocityX, gyro.AngularVelocityY, gyro.AngularVelocityZ);
-            System.Diagnostics.Debug.WriteLine(text);
-        }
-
 
     }
 }
---
+++ Samples/Accelerometer/Accelerometer.Universal/BandAPI.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Band;
+using Microsoft.Band.Sensors;
+using Windows.UI.Xaml;
+
+
+
+namespace Accelerometer
+{
+    public class BandAPI
+    {
+        public enum VibrateBand: int {Win = 1, HitBall = 2};
+
+        private static double accel = 0;
+
+        private static IBandClient bandClient;
+        private static BandAPI instance;
+
+        public static BandAPI Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new BandAPI();
+                }
+                return instance;
+            }
+        }
+
+        public async void StartBandAPI()
+        {
+            
+            try
+            {
+                // Get the list of Microsoft Bands paired to the device.
+                IBandInfo[] pairedBands = await BandClientManager.Instance.GetBandsAsync();
+                if (pairedBands.Length < 1)
+                {
+                    string needBand = "Need to pair Band";
+                    System.Diagnostics.Debug.WriteLine(needBand);
+                    return;
+                }
+
+                // Connect to Microsoft Band.
+                using (bandClient = await BandClientManager.Instance.ConnectAsync(pairedBands[0]))
+                {
+
+                    // Subscribe to Accelerometer data.
+                    bandClient.SensorManager.Accelerometer.ReadingChanged += (s, args) =>
+                    {
+                        accel = Accelerometer_ReadingChanged(s, args);
+                    };
+                    System.Diagnostics.Debug.WriteLine("Retrieving accelerometer data");
+                    await bandClient.SensorManager.Accelerometer.StartReadingsAsync();
+                    // Keep retrieving Accelerometer data for an hour
+                    await Task.Delay(TimeSpan.FromHours(1));
+                    await bandClient.SensorManager.Accelerometer.StopReadingsAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.ToString());
+            }
+        }
+
+        public async void StopBandAPI(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await bandClient.SensorManager.Accelerometer.StopReadingsAsync();
+                System.Diagnostics.Debug.WriteLine("Stop retrieving accelerometer data");
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.ToString());
+            }
+        }
+
+        private double Accelerometer_ReadingChanged(object sender, BandSensorReadingEventArgs<IBandAccelerometerReading> e)
+        {
+            IBandAccelerometerReading accel = e.SensorReading;
+            //Read in accelerometer data from sensor
+            string text = string.Format("Accelerometer X = {0:G4}, Y = {1:G4}, Z = {2:G4}", accel.AccelerationX, accel.AccelerationY, accel.AccelerationZ);
+            System.Diagnostics.Debug.WriteLine(text);
+            return Math.Abs(Math.Max(accel.AccelerationX, Math.Max(accel.AccelerationY, accel.AccelerationZ)));
+        }
+
+        public static double getAcceleration() {
+            return accel;
+        }
+
+        
+        public static void vibrateBand(VibrateBand vibrationType) {
+            switch(vibrationType)
+            {
+                case VibrateBand.Win:
+                    bandClient.NotificationManager.VibrateAsync(Microsoft.Band.Notifications.VibrationType.ThreeToneHigh);
+                    break;
+                case VibrateBand.HitBall:
+                    bandClient.NotificationManager.VibrateAsync(Microsoft.Band.Notifications.VibrationType.OneToneHigh);
+                    break;
+            }
+        }
+
+
+
+    }
+}
