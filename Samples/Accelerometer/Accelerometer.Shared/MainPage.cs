c1ae914c069b7766227178998479452803009df1
Wei Low <weiwlow@gmail.com>
2016-07-25 23:49:55 -0700

    haptic feedback if specific acceleration achieved

--- Samples/Accelerometer/Accelerometer.Shared/MainPage.cs
+++ Samples/Accelerometer/Accelerometer.Shared/MainPage.cs
@@ -37,7 +37,7 @@ namespace Accelerometer
 
             try
             {
-                // Get the list of Microsoft Bands paired to the phone.
+                // Get the list of Microsoft Bands paired to the device.
                 IBandInfo[] pairedBands = await BandClientManager.Instance.GetBandsAsync();
                 if (pairedBands.Length < 1)
                 {
@@ -51,17 +51,21 @@ namespace Accelerometer
                     int samplesReceived = 0; // the number of Accelerometer samples received
 
                     // Subscribe to Accelerometer data.
-                    bandClient.SensorManager.Accelerometer.ReadingChanged += (s, args) => 
+                    bandClient.SensorManager.Accelerometer.ReadingChanged += (s, args) =>
                     {
                         samplesReceived++;
-                        Accelerometer_ReadingChanged(s, args);
+                        int accel = Accelerometer_ReadingChanged(s, args);
+                        if (accel == 2)
+                            bandClient.NotificationManager.VibrateAsync(Microsoft.Band.Notifications.VibrationType.NotificationOneTone);
+                        else
+                        { }
                     };
+
                     await bandClient.SensorManager.Accelerometer.StartReadingsAsync();
-                    
+
                     // Receive Accelerometer data for a while, then stop the subscription.
-                    await Task.Delay(TimeSpan.FromSeconds(1));
+                    await Task.Delay(TimeSpan.FromSeconds(20));
                     await bandClient.SensorManager.Accelerometer.StopReadingsAsync();
-                    this.viewModel.StatusMessage = string.Format("{0} Accelerometer samples were collected in 1 second.", samplesReceived);
                 }
             }
             catch (Exception ex)
@@ -70,10 +74,29 @@ namespace Accelerometer
             }
         }
 
-        private void Accelerometer_ReadingChanged(object sender, BandSensorReadingEventArgs<IBandAccelerometerReading> e)
+        private int Accelerometer_ReadingChanged(object sender, BandSensorReadingEventArgs<IBandAccelerometerReading> e)
         {
+            int v;
             IBandAccelerometerReading accel = e.SensorReading;
-            string text = string.Format("X = {0:G4}\nY = {1:G4}\nZ = {2:G4}", accel.AccelerationX, accel.AccelerationY, accel.AccelerationZ);
+            //Read in accelerometer data from sensor
+            string text = string.Format("Accelerometer X = {0:G4}, Y = {1:G4}, Z = {2:G4}", accel.AccelerationX, accel.AccelerationY, accel.AccelerationZ);
+            System.Diagnostics.Debug.WriteLine(text);
+            if (Math.Abs(accel.AccelerationX) > 2.0)
+            {
+                v = 2;
+            }
+            else
+            {
+                v = 1;
+            }
+            return v;
+        }
+
+        private void Gyroscope_ReadingChanged(object sender, BandSensorReadingEventArgs<IBandGyroscopeReading> e)
+        {
+            IBandGyroscopeReading gyro = e.SensorReading;
+            //Read in gyroscope data from sensor
+            string text = string.Format("Gyroscope: X = {0:G4}, Y = {1:G4}, Z = {2:G4}", gyro.AngularVelocityX, gyro.AngularVelocityY, gyro.AngularVelocityZ);
             System.Diagnostics.Debug.WriteLine(text);
         }
 
